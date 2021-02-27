d1449d6a653793a5646bf857cbda6e557638afed
814657780 <18037818812@163.com>
2021-02-27 22:45:38 +0800

    “typescript”

---
+++ chapter01/02_basis.ts
@@ -0,0 +1,13 @@
+// 声明一个变量a，同时指定它的类型为number
+let a: number;
+
+// a的类型设置为number，在以后的使用过程中a的值只能是数字
+a = 10;
+a = 33;
+// a = "hello"; // 此行代码会报错，因为变量a的类型是number，不能赋值字符串
+
+let b:string;
+b = 'hello';
+// b = 123;
+
+// 
