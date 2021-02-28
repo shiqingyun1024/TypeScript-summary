753316130f6945d3e74fa7831071866d3b92acac
814657780 <18037818812@163.com>
2021-02-28 09:33:10 +0800

    “typescript”

--- chapter01/02_basis.ts
+++ chapter01/02_basis.ts
@@ -10,4 +10,20 @@ let b:string;
 b = 'hello';
 // b = 123;
 
-// 
+// 如果变量的声明和赋值是同时进行的，TS可以自动对变量进行类型检测。
+let c = true;  // 赋值的时候已经确定类型（第一次赋值的时候自动赋值类型），那么在后面再赋值的时候就不能赋值其他类型了，会报错
+//  c = '123';
+
+// js中的函数是不考虑参数的类型和个数的
+// function sum(a,b){
+//    return a + b;
+// }
+// console.log(sum(123,456))  // 579
+// console.log(sum(123,'456'))  // '123456'
+
+// 指定参数的类型和返回值得类型
+function sum(a:number,b:number):number{
+    return a + b;
+}
+sum(123,456) // 579
+
