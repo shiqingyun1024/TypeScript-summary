6e807b0b4168869dbd53a93cce66a4a1fb506d0f
814657780 <18037818812@163.com>
2021-03-02 22:01:50 +0800

    “typescript”

--- chapter01/part2/src/app.ts
+++ chapter01/part2/src/app.ts
@@ -6,3 +6,21 @@ let b:myNum = 4;
 // function fn(a,b){
 //     return a + b
 // }
+
+// 下面是不明确类型的this的情况
+// function fn2(){
+//     alert(this)
+// }
+
+
+// 下面是严格的检查空值的情况
+// let box1 = document.getElementById('box1');
+// box1.addEventListener('click',function(){
+//     alert("检查空值");
+// })
+// 上面这种情况，可以用链判断运算符 ?.
+let box1 = document.getElementById('box1');
+box1?.addEventListener('click',function(){
+    alert("检查空值");
+})
+
