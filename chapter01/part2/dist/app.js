78b8a40d86e146d875d6065c7bdb981213080029
814657780 <18037818812@163.com>
2021-03-02 21:39:30 +0800

    “typescript”

--- chapter01/part2/dist/app.js
+++ chapter01/part2/dist/app.js
@@ -1,2 +1,5 @@
 let a = '123';
 let b = 4;
+function fn(a, b) {
+    return a + b;
+}
--- chapter01/part2/src/app.ts
+++ chapter01/part2/src/app.ts
@@ -1,3 +1,8 @@
 let a:string = '123';
 type myNum = 1|2|3|4;
 let b:myNum = 4;
+
+// 下面就是隐式any的情况
+// function fn(a,b){
+//     return a + b
+// }
