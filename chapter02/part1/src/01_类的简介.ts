8de8014fb9038315404d5087e9d55d7bc685a753
814657780 <18037818812@163.com>
2021-03-04 23:01:02 +0800

    “typescript”

--- chapter02/part1/src/01_类的简介.ts
+++ chapter02/part1/src/01_类的简介.ts
@@ -14,6 +14,8 @@ class Person{
      * 使用static开头的属性是静态属性（类属性），可以直接通过类去访问
      *     Person.age
      * 
+     * readonly开头的属性本身一个只读的属性，无法修改
+     * 
      * */ 
 
     // 定义实例属性
@@ -22,9 +24,16 @@ class Person{
     // 在属性前使用static关键词可以定义类属性（静态属性）只能类本身访问的属性
     static age:number = 18;
 
+    // readonly表示只读的属性 
+    readonly home:string = 'SQ' // 祖籍商丘
+
+    // 同时也可以组合使用 static必须在前面
+    static readonly sex:string = 'male';
+
 }
 
 const per = new Person();
 console.log(per.name);
 console.log(Person.age);
+// per.home = "ss" 只读，不能修改
 
