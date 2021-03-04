13408d2f9b83eb9c5ef6db77acc1f34a1725b4c9
814657780 <18037818812@163.com>
2021-03-04 23:20:47 +0800

    “typescript”

--- chapter02/part1/dist/02_构造函数.js
+++ chapter02/part1/dist/02_构造函数.js
@@ -1,1 +1,15 @@
 "use strict";
+class Dog {
+    // name = "旺财";
+    // age = 3;
+    // constructor 被称为构造函数
+    // 构造函数会在对象创建是调用
+    constructor() {
+        console.log('构造函数执行了~~~~');
+    }
+    bark() {
+        console.log("汪汪汪");
+    }
+}
+const dog = new Dog();
+console.log(dog);
--- chapter02/part1/src/02_构造函数.ts
+++ chapter02/part1/src/02_构造函数.ts
@@ -1,6 +1,13 @@
 class Dog{
-    name = "旺财";
-    age = 3;
+    // name = "旺财";
+    // age = 3;
+
+    // constructor 被称为构造函数
+    // 构造函数会在对象创建是调用
+    constructor(){
+        console.log('构造函数执行了~~~~');
+        
+    }
 
     bark(){
         console.log("汪汪汪");
@@ -9,3 +16,4 @@ class Dog{
 }
 
 const dog = new Dog();
+console.log(dog);
