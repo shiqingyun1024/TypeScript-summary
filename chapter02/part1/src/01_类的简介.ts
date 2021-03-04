858a5d642e45e72578ed10fece38797d7d28519d
814657780 <18037818812@163.com>
2021-03-04 22:44:45 +0800

    “typescript”

--- chapter02/part1/src/01_类的简介.ts
+++ chapter02/part1/src/01_类的简介.ts
@@ -9,11 +9,13 @@ class Person{
 
     // 定义实例属性
     name:string = "孙悟空";
-    age:number = 18;
+
+    // 在属性前使用static关键词可以定义类属性（静态属性）只能类本身访问的属性
+    static age:number = 18;
 
 }
 
 const per = new Person();
-console.log(per);
-console.log();
+console.log(per.name);
+console.log(Person.age);
 
