0147b89087e6cfdecc55ca1a0b2ece82692870fe
814657780 <18037818812@163.com>
2021-03-04 22:40:02 +0800

    “typescript”

--- chapter02/part1/src/01_类的简介.ts
+++ chapter02/part1/src/01_类的简介.ts
@@ -1,7 +1,19 @@
 // 使用class关键字来定义一个类
+/**
+ * 对象中主要包含了两个部分：
+ *    属性
+ *    对象
+ * 
+ * */ 
 class Person{
+
+    // 定义实例属性
+    name:string = "孙悟空";
+    age:number = 18;
 
 }
 
 const per = new Person();
 console.log(per);
+console.log();
+
