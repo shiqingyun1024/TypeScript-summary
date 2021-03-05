657dd9991afb3d834d0a71a7bdf5df04b646c3b0
814657780 <18037818812@163.com>
2021-03-05 22:43:56 +0800

    “typescript”

--- chapter02/part1/dist/03_继承.js
+++ chapter02/part1/dist/03_继承.js
@@ -10,6 +10,20 @@
             console.log('汪汪汪');
         }
     }
+    // 定义一个表示猫的类
+    class Cat {
+        constructor(name, age) {
+            this.name = name;
+            this.age = age;
+        }
+        sayHello() {
+            console.log('喵喵喵');
+        }
+    }
     const dog = new Dog('旺财', 5);
+    const cat = new Cat('咪咪', 3);
     console.log(dog);
+    console.log(cat);
+    console.log(dog.sayHello());
+    console.log(cat.sayHello());
 })();
--- chapter02/part1/src/03_继承.ts
+++ chapter02/part1/src/03_继承.ts
@@ -12,6 +12,24 @@
             console.log('汪汪汪')
         }
     }
+    // 定义一个表示猫的类
+    class Cat{
+        name:string;
+        age:number;
+        constructor(name:string,age:number){
+            this.name = name;
+            this.age = age;
+        }
+
+        sayHello(){
+            console.log('喵喵喵')
+        }
+    }
     const dog = new Dog('旺财',5);
+    const cat = new Cat('咪咪',3);
     console.log(dog)
+    console.log(cat)
+    console.log(dog.sayHello());
+    console.log(cat.sayHello());
+    
 })()
