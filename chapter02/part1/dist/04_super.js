1656c53e1625c1df66060a44648776526f4860cd
814657780 <18037818812@163.com>
2021-03-05 23:33:23 +0800

    “typescript”

---
+++ chapter02/part1/dist/04_super.js
@@ -0,0 +1,17 @@
+"use strict";
+(function () {
+    class Animal {
+        constructor(name) {
+            this.name = name;
+        }
+        sayHello() {
+            console.log('动物在叫~~');
+        }
+    }
+    class Dog extends Animal {
+        sayHello() {
+            // 在类的方法中 super就表示当前类的父类
+            super.sayHello();
+        }
+    }
+})();
