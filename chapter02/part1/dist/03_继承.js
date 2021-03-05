f0654df13c4be430fc44889672a003038efb5d31
814657780 <18037818812@163.com>
2021-03-05 23:19:10 +0800

    “typescript”

--- chapter02/part1/dist/03_继承.js
+++ chapter02/part1/dist/03_继承.js
@@ -1,24 +1,34 @@
 "use strict";
 (function () {
-    // 定义一个表示狗的类
-    class Dog {
+    // 定义一个表示动物的类
+    class Animal {
         constructor(name, age) {
             this.name = name;
             this.age = age;
         }
         sayHello() {
-            console.log('汪汪汪');
+            console.log('动物');
+        }
+    }
+    /**
+     * Dog extends Animal
+     *  --此时 Animal被称为父类，Dog被称为子类
+     *  --使用继承后，子类将会拥有父类所有的属性和方法
+     *  --通过继承可以将多个类中的共有的代码写在一个父类中
+     *    --这样只需要写一次即可让所有的子类都同时拥有父类中的属性和方法
+     *    --如果希望在子类中添加一些父类中没有的属性或方法直接添加就行
+     *
+     *
+     *
+     * */
+    // 定义一个表示狗的类
+    class Dog extends Animal {
+        run() {
+            console.log(`${this.name}在跑~~~`);
         }
     }
     // 定义一个表示猫的类
-    class Cat {
-        constructor(name, age) {
-            this.name = name;
-            this.age = age;
-        }
-        sayHello() {
-            console.log('喵喵喵');
-        }
+    class Cat extends Animal {
     }
     const dog = new Dog('旺财', 5);
     const cat = new Cat('咪咪', 3);
