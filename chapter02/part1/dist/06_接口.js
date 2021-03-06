05a96d69cacd4e1be873e3ec1bf0752b6ef37d99
814657780 <18037818812@163.com>
2021-03-06 23:22:13 +0800

    “typescript”

--- chapter02/part1/dist/06_接口.js
+++ chapter02/part1/dist/06_接口.js
@@ -11,4 +11,16 @@
         age: 11,
         gender: '男'
     };
+    /**
+     * 定义类时，可以使类去实现一个接口
+     *     实现接口就是使类满足接口的要求，其实说白了，接口就是一个规范，使用时类满足这个规范即可
+     * */
+    class MyClass {
+        constructor(name) {
+            this.name = name;
+        }
+        sayHello() {
+            console.log("接口");
+        }
+    }
 })();
--- chapter02/part1/src/06_接口.ts
+++ chapter02/part1/src/06_接口.ts
@@ -40,5 +40,19 @@
         sayHello():void;
     }
 
+    /**
+     * 定义类时，可以使类去实现一个接口
+     *     实现接口就是使类满足接口的要求，其实说白了，接口就是一个规范，使用时类满足这个规范即可
+     * */ 
+    class MyClass implements myInterface2{
+        name:string;
+        constructor(name:string){
+            this.name = name
+        }
+        sayHello(){
+            console.log("接口");
+        }
+    }
+
 
 })();
