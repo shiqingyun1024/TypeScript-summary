0a7f8155dde373512bf1d32541546b28f1fcffb2
814657780 <18037818812@163.com>
2021-03-06 22:26:59 +0800

    “typescript”

--- chapter02/part1/dist/05_抽象类.js
+++ chapter02/part1/dist/05_抽象类.js
@@ -2,20 +2,19 @@
 (function () {
     /**
      * 以abstract开头的类是抽象类。
-     * 抽象类和
+     *   抽象类和其他类区别不大，只是不能用来创建对象
+     *   抽象类就是专门用来被继承的类。
      *
+     *   抽象类中可以添加抽象方法
      * */
     class Animal {
         constructor(name) {
             this.name = name;
         }
-        sayHello() {
-            console.log('动物在叫~~');
-        }
     }
     class Dog extends Animal {
         sayHello() {
-            super.sayHello();
+            console.log("汪汪汪！");
         }
     }
     let dog = new Dog("旺财");
