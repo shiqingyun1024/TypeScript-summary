ea187ed65d9f381f9776cd7f93a31e03c6c27219
814657780 <18037818812@163.com>
2021-03-06 23:52:18 +0800

    “typescript”

--- chapter02/part1/dist/07_属性的封装.js
+++ chapter02/part1/dist/07_属性的封装.js
@@ -3,8 +3,8 @@
     // 定义一个表示人的类
     class Person {
         constructor(name, age) {
-            this.name = name;
-            this.age = age;
+            this._name = name;
+            this._age = age;
         }
     }
     const per = new Person('孙悟空', 18);
@@ -12,7 +12,7 @@
      * 现在属性是在对象中设置的，属性可以任意的被修改
      *    属性可以任意被修改将会导致对象中的数据变得非常不安全
      * */
-    per.name = '猪八戒';
-    per.age = 20;
+    per._name = '猪八戒';
+    per._age = 20;
     console.log(per);
 })();
--- chapter02/part1/src/07_属性的封装.ts
+++ chapter02/part1/src/07_属性的封装.ts
@@ -1,12 +1,17 @@
 (function () {
     // 定义一个表示人的类
     class Person {
-        name: string;
-        age: number;
+        // TS可以在属性前添加属性的修饰符
+        /**
+         * public 修饰的属性可以在任意位置访问（修改）同时也是默认值
+         * 
+         * */ 
+        _name: string;
+        _age: number;
 
         constructor(name: string, age: number) {
-            this.name = name;
-            this.age = age;
+            this._name = name;
+            this._age = age;
         }
     }
 
@@ -16,8 +21,8 @@
      * 现在属性是在对象中设置的，属性可以任意的被修改
      *    属性可以任意被修改将会导致对象中的数据变得非常不安全
      * */ 
-    per.name = '猪八戒';
-    per.age = 20;
+    per._name = '猪八戒';
+    per._age = 20;
     console.log(per);
     
 })()
