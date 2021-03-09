3d06e5e9bf92b031ba84fa91855b43c7a0b963fa
814657780 <18037818812@163.com>
2021-03-09 22:29:57 +0800

    “typescript”

--- chapter02/part2/src/index.ts
+++ chapter02/part2/src/index.ts
@@ -21,9 +21,24 @@ class Food{
     get Y(){
         return this.element.offsetTop;
     }
+
+    // 修改食物的位置
+    change(){
+        // 生成一个随机的位置
+        // 食物的位置最小是0，最大是290
+        // 蛇移动一次就是一格，一格的大小是10，所以就要求食物的坐标必须是整10
+        let top = Math.round(Math.random()*29)*10;
+        let left = Math.round(Math.random()*29)*10;
+
+        this.element.style.left = left+'px';
+        this.element.style.top = top+'px';
+    }
 }
 
 // 测试代码
 const food = new Food();
 console.log(food.X,food.Y);
+food.change();
+console.log(food.X,food.Y);
+
 
