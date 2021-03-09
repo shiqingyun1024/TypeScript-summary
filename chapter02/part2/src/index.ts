20ba79e09a241c05d5f3d93a99185f0ab3aa60eb
814657780 <18037818812@163.com>
2021-03-09 23:05:22 +0800

    “typescript”

--- chapter02/part2/src/index.ts
+++ chapter02/part2/src/index.ts
@@ -53,11 +53,14 @@ class ScorePanel{
 
     // 设置一个变量限制等级
     maxLevel:number;
+    // 设置一个变量表示多少分时升级
+    upScore:number;
 
-    constructor(maxLevel:number = 10){
+    constructor(maxLevel:number = 10,upScore:number = 10){
         this.scoreEle = document.getElementById('score')!;
         this.levelEle = document.getElementById('level')!;
         this.maxLevel = maxLevel;
+        this.upScore = upScore;
     }
 
     // 设置一个加分的方法
@@ -66,7 +69,7 @@ class ScorePanel{
         // this.score++;
         this.scoreEle.innerHTML = ++this.score + '';
         // 判断分数是多少，根据分数来判断是否升级
-        if(this.score % 10 === 0){
+        if(this.score % this.upScore === 0){
            this.levelUp();
         }
     }
@@ -81,8 +84,11 @@ class ScorePanel{
 }
 
 const scorePanel = new ScorePanel();
-scorePanel.addScore();
-scorePanel.addScore();
-scorePanel.addScore();
+for(let i = 0; i < 10; i++){
+    scorePanel.addScore();
+}
+
+// scorePanel.addScore();
+// scorePanel.addScore();
 
 
