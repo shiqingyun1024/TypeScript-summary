658f6f32f5f95a9f3641f23fa7541a2947f77328
814657780 <18037818812@163.com>
2021-03-10 22:33:27 +0800

    “typescript”

--- chapter02/part2/src/modules/GameControl.ts
+++ chapter02/part2/src/modules/GameControl.ts
@@ -5,18 +5,20 @@ import ScorePanel from "./ScorePanel";
 
 
 // 游戏控制器，控制其他的所有类
-class GameControl{
+class GameControl {
     // 定义三个属性
     // 蛇
-    snake:Snake;
+    snake: Snake;
     // 食物
-    food:Food;
+    food: Food;
     // 记分牌
-    scorePanel:ScorePanel;
+    scorePanel: ScorePanel;
     // 创建一个属性来存储蛇的移动方向（也就是按键的方向）
-    direction:string="";
+    direction: string = "";
+    // 创建一个属性用来记录游戏是否结束
+    isLive = true;
 
-    constructor(){
+    constructor() {
         this.snake = new Snake();
         this.food = new Food();
         this.scorePanel = new ScorePanel();
@@ -24,33 +26,69 @@ class GameControl{
     }
 
     // 游戏的初始化方法，调用后游戏即开始
-    init(){
+    init() {
         // 绑定键盘按键按下的事件 this.keydownHandler.bind(this)为什么这么写呢，因为keydownHandler谁调用，this指向谁
         // 所以第一个this指向的是document，所以用bind的方法绑定，小括号中的this指向的就是当前的这个class类了，所以这个时候keydownHandler函数里面的this指向的就是GameControl这个类了
-        document.addEventListener('keydown',this.keydownHandler.bind(this));
+        document.addEventListener('keydown', this.keydownHandler.bind(this));
+        // 调用run方法，使蛇移动
+        this.run();
     }
     /**
      * ArrowUp Up
      * ArrowDown Down
      * ArrowLeft Left
      * ArrowRight Right
-     * */ 
+     * */
 
     // 创建一个键盘按下的响应函数
-    keydownHandler(event:KeyboardEvent){
+    keydownHandler(event: KeyboardEvent) {
         // 需要检查event.key的值是否合法（用户是否按了正确的按键）
         this.direction = event.key;
     }
 
     // 创建一个控制蛇移动的方法
-    run(){
+    run() {
         /**
          * 根据方向（this.direction）来使蛇的位置改变
          *    向上  top减少
          *    向下  top增加
          *    向左  left减少
          *    向右  left增加
-         * */ 
+         * */
+        // 获取蛇现在的坐标
+        let X = this.snake.X;
+        let Y = this.snake.Y;
+
+        // 根据按键方向来修改X值和Y值
+        switch (this.direction) {
+            case "ArrowUp":
+            case "Up":
+                // 向上移动 top减少
+                Y -= 10;
+                break;
+            case "ArrowDown":
+            case "Down":
+                // 向下移动 top增加
+                Y += 10;
+                break;
+            case "ArrowLeft":
+            case "Left":
+                // 向左移动 left减少
+                X -= 10;
+                break;
+            case "ArrowRight":
+            case "Right":
+                // 向右移动 left增加
+                X += 10;
+                break;
+        }
+
+        // 给蛇重新定位赋值
+        this.snake.X = X;
+        this.snake.Y = Y;
+
+        // 开启一个定时器
+        this.isLive && setTimeout(this.run.bind(this),300 - (this.scorePanel.level - 1)*30)
     }
 }
 
