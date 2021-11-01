571f506739ef655717cf9924e38dcf0b74566dd9
814657780 <18037818812@163.com>
2021-11-01 20:37:16 +0800

    ts项目实战

---
+++ TODO-TS/src/js/TodoEvent.ts
@@ -0,0 +1,33 @@
+import { ITodoData } from "./typings";
+
+class TodoEvent {
+    private todoData: ITodoData[];
+    constructor(todoData: ITodoData[]) {
+        this.todoData = todoData
+    }
+
+    public addTodo(todo:ITodoData): undefined | number {
+        const _todo:null | ITodoData = this.todoData.find((item:ITodoData)=> item.content);
+        if(!_todo){
+            this.todoData.push(todo);
+            return;
+        }
+        return 1001;
+
+    }
+    public removeTodo(id:number):void {
+        this.todoData = this.todoData.filter((todo:ITodoData)=>todo.id !== id)
+
+    }
+    public toggleComplete(id:number):void {
+        this.todoData = this.todoData.map((todo:ITodoData)=>{
+            if(todo.id === id){
+                todo.completed = !todo.completed;
+            }
+            return todo;
+        })
+
+    }
+}
+
+export default TodoEvent;
