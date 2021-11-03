caa05bb3db5f4bc18a5bed78e4ecf301bed271c6
814657780 <18037818812@163.com>
2021-11-03 22:11:53 +0800

    ts项目实战后端搭建

--- TODO-TS/server/app.ts
+++ TODO-TS/server/app.ts
@@ -6,8 +6,30 @@ const app: Application = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+console.log('Welcome to EXPRESS');
+
 app.all('*',(req,res,next)=>{
     res.header('Access-Control-Allow-Origin','*');
     res.header('Access-Control-Allow-methods','POST,GET,PUT,DELELTE,OPTIONS');
     next();
+})
+
+app.get('./todolist',function(req,res){
+
+})
+
+app.post('./toggle',function(req,res){
+    
+})
+
+app.post('./remove',function(req,res){
+    
+})
+
+app.post('./add',function(req,res){
+    
+})
+
+app.listen(8080, function(){
+    console.log('Listening on port 8080');
 })
