fe2170fcfa107757fcaa88ee30b72c30224c69b4
814657780 <18037818812@163.com>
2021-11-04 22:02:11 +0800

    ts项目实战后端搭建

--- TODO-TS/src/js/TodoService.ts
+++ TODO-TS/src/js/TodoService.ts
@@ -1,9 +1,21 @@
 import $ from 'jquery';
+import { ITodoData } from './typings';
 
 export function getTodoList(
     target: any,
     methodName: string,
     descriptor: PropertyDecorator
 ){
-  console.log(target,methodName,descriptor);
+//   console.log(target,methodName,descriptor);
+  const _origin = descriptor.value;
+  descriptor.value = function(todoData:ITodoData[]){
+      $.get('http://localhost:8080/todolist').then((res:string)=>{
+          if(!res){
+              return;
+          }
+          todoData = JSON.parse(res);
+      }).then(()=>{
+          _origin.call(this,todoData)
+      })
+  }
 }
