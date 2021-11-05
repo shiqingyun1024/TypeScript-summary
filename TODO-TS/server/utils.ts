e2d9186d7419ef07e9bf2282b05571cfddc72bc9
814657780 <18037818812@163.com>
2021-11-05 22:32:25 +0800

    ts项目实战后端搭建

--- TODO-TS/server/utils.ts
+++ TODO-TS/server/utils.ts
@@ -1,9 +1,21 @@
 import { readFileSync,writeFileSync } from 'fs';
 import { resolve } from 'path';
+import { ITodoData } from '../src/js/typings';
+
+// 读取文件
 export function readFile (path:string):string{
     return readFileSync(resolve(__dirname,path),'utf8')
 }
 
+// 写入文件
 export function writeFile<T>(path:string,data:T):void{
     writeFileSync(resolve(__dirname,path),JSON.stringify(data))
+}
+
+// 文件操作
+export function fileOperation(path:string,fn?:any): string | void{
+    let todoList: ITodoData[]= JSON.parse(readFile('todo.json') || '[]');
+    if(!fn){
+        return JSON.stringify(todoList);
+    }
 }
