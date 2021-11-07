cecd9856bf8893ef5be62d30fb375c6663e03306
814657780 <18037818812@163.com>
2021-11-07 22:29:57 +0800

    vue3.0+TypeScript

--- TODO-vue3.0/vue3-todo/src/components/TodoInput/index.vue
+++ TODO-vue3.0/vue3-todo/src/components/TodoInput/index.vue
@@ -9,11 +9,22 @@
 </template>
 
 <script lang="ts">
+// 单个数据的话，一般用ref，多个数据的话一般用reactive
 import { defineComponent,reactive,ref } from 'vue';
 export default defineComponent({
   name: "TodoInput",
   setup(){
-    
+    const todoValue = ref<string>('')
+    const setTodoValue = (e:KeyboardEvent)=>{
+      if(e.keyCode === 13 && todoValue.value.trim().length){
+        // 设置数据
+        todoValue.value = '';
+      }
+    }
+    return {
+      todoValue,
+      setTodoValue
+    }
   }
 });
 </script>
