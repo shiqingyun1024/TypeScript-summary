f4792d02e514a24fa0f33f4ef9d5023fb77ff156
814657780 <18037818812@163.com>
2021-11-07 22:41:45 +0800

    vue3.0和TypeScript组成的todolist

--- TODO-vue3.0/vue3-todo/src/store/state.ts
+++ TODO-vue3.0/vue3-todo/src/store/state.ts
@@ -1,3 +1,15 @@
 export default {
-    
+    list:[]
 }
+/**
+ * list:ITodo[]
+ * 
+ * ITodo
+ * listItem:{
+ *   id:new Date().getTime -> number
+ *   content: string,
+ *   status: finished/doing/willdo  -> 枚举
+ * }
+ * 
+ * 对象的定义用type 类型 或者interface 接口，用interface的比较多点，因为interface可以继承（extends）其他的接口
+ * **/ 
