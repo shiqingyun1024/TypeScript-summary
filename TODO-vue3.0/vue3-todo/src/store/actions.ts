1421adbc568da1c2f22201f597a7dac37d2d8860
814657780 <18037818812@163.com>
2021-11-08 22:57:03 +0800

    vue3.0和TypeScript组成的todolist

--- TODO-vue3.0/vue3-todo/src/store/actions.ts
+++ TODO-vue3.0/vue3-todo/src/store/actions.ts
@@ -1,7 +1,13 @@
+import { IState, ITodo } from "@/typings";
+import { Commit } from "vuex";
 import { SET_TODO } from "./actionTypes";
 
+interface ICtx{
+    commit:Commit,
+    state:IState
+}
 export default {
-    [SET_TODO](ctx,){
-        
+    [SET_TODO]({commit,state}:ICtx,todo:ITodo):void{
+      commit(SET_TODO,todo);
     }
 }
