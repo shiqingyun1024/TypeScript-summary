6d8a467a182b989718c0b1d89dcbbfa579a07cdc
814657780 <18037818812@163.com>
2021-11-09 22:09:47 +0800

    vue3.0和TypeScript组成的todolist

--- TODO-vue3.0/vue3-todo/src/hooks/index.ts
+++ TODO-vue3.0/vue3-todo/src/hooks/index.ts
@@ -1,13 +1,19 @@
+import { ITodo, TODO_STATUS } from "@/typings"
+
 interface IUseTodo {
-    setTodo:()=>void;
+    setTodo:(value:string)=>void;
     setTodoList:()=>void;
     removeTodo:()=>void;
     setStatus:()=>void;
     setDoing:()=>void;
 }
 function useTodo():IUseTodo {
-    function setTodo(){
-
+    function setTodo(value:string):void{
+       const todo:ITodo = {
+           id: new Date().getTime(),
+           content:value,
+           status:TODO_STATUS.WILLDO
+       }
     }
     function setTodoList(){
         
