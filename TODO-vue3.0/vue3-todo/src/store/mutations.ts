f1f57e2b2655ec872d713049ac0b1e92248a9d3f
814657780 <18037818812@163.com>
2021-11-11 22:07:26 +0800

    vue3.0和TypeScript组成的todolist

--- TODO-vue3.0/vue3-todo/src/store/mutations.ts
+++ TODO-vue3.0/vue3-todo/src/store/mutations.ts
@@ -4,7 +4,7 @@ export default {
     [SET_TODO](state:IState, todo:ITodo):void{
         state.list.unshift(todo);
         console.log(state.list);
-    }
+    },
     [SET_TODO_LIST](state:IState, todoList:ITodo[]):void{
         state.list = todoList;
     }
