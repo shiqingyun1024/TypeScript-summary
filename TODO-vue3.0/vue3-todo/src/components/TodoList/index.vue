75cbebf352aaaee69dc756091831b37cf2d4a0fc
814657780 <18037818812@163.com>
2021-11-12 22:39:39 +0800

    vue3.0和TypeScript组成的todolist

--- TODO-vue3.0/vue3-todo/src/components/TodoList/index.vue
+++ TODO-vue3.0/vue3-todo/src/components/TodoList/index.vue
@@ -1,26 +1,34 @@
 <template>
   <div>
-    <todo-item 
-     v-for="item of todoList"
-     :key = "item.id"
-     :item = "item"
-     @removeTodo="removeTodo"
-     @setStatus="setStatus"
-     @setDoing="setDoing"
+    <todo-item
+      v-for="item of todoList"
+      :key="item.id"
+      :item="item"
+      @removeTodo="removeTodo"
+      @setStatus="setStatus"
+      @setDoing="setDoing"
     />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
-import TodoItem from './Item.vue';
+import { defineComponent, onMounted, PropType } from "vue";
+import { IUseTodo, useTodo } from "../../hooks";
+import TodoItem from "./Item.vue";
 export default defineComponent({
   name: "TodoList",
-  props:{
-    todoList:Array as PropType<ITodo[]>
+  props: {
+    todoList: Array as PropType<ITodo[]>,
   },
   components: {
-    TodoItem
+    TodoItem,
+  },
+  setup() {
+    const {
+      removeTodo,
+      setStatus,
+      setDoing
+    }:IUseTodo = useTodo();
   },
 });
 </script>
