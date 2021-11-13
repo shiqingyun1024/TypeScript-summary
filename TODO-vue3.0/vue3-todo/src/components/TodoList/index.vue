62108bea38af2f705f701de075668c55dc0db6c9
814657780 <18037818812@163.com>
2021-11-13 22:20:00 +0800

    vue3.0和TypeScript组成的todolist

--- TODO-vue3.0/vue3-todo/src/components/TodoList/index.vue
+++ TODO-vue3.0/vue3-todo/src/components/TodoList/index.vue
@@ -14,6 +14,7 @@
 <script lang="ts">
 import { defineComponent, onMounted, PropType } from "vue";
 import { IUseTodo, useTodo } from "../../hooks";
+import { ITodo } from '../../typings';
 import TodoItem from "./Item.vue";
 export default defineComponent({
   name: "TodoList",
--- TODO-vue3.0/vue3-todo/src/components/TodoList/item.vue
+++ TODO-vue3.0/vue3-todo/src/components/TodoList/item.vue
@@ -1,39 +1,45 @@
 <template>
-<div>
-  <input 
-    type="checkbox"
-    :checked = "item.status === FINISHED"
-    @click="setStatus(item.id)"
-  />
-  <span
-  :class="item.status === FINISHED?'line-through':''"
-  >{{item.content}}</span>
-  <button @click="removeTodo(item.id)">删除</button>
-  <button 
-    v-if="item.status!==FINISHED"
-    @click="setDoing(item.id)"
-    :class="item.status===DOING?'doing':'willdo'"
-    >{{item.status===DOING?'正在做...':'马上做'}}</button>
-</div>
+  <div>
+    <input
+      type="checkbox"
+      :checked="item.status === FINISHED"
+      @click="setStatus(item.id)"
+    />
+    <span :class="item.status === FINISHED ? 'line-through' : ''">{{
+      item.content
+    }}</span>
+    <button @click="removeTodo(item.id)">删除</button>
+    <button
+      v-if="item.status !== FINISHED"
+      @click="setDoing(item.id)"
+      :class="item.status === DOING ? 'doing' : 'willdo'"
+    >
+      {{ item.status === DOING ? "正在做..." : "马上做" }}
+    </button>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { ITodo } from '../../typings';
+import { defineComponent, PropType } from "vue";
 export default defineComponent({
   name: "TodoItem",
+  props: {
+    item: Object as PropType<ITodo>,
+  },
 });
 </script>
 
 <style>
-.line-through{
+.line-through {
   text-decoration: line-through;
 }
-.doing{
-   background-color: #cdcdcd;
-   color: #ccc;
+.doing {
+  background-color: #cdcdcd;
+  color: #ccc;
 }
-.willdo{
+.willdo {
   background-color: orange;
-   color: #fff;
+  color: #fff;
 }
 </style>
