51370bdf9ae53576ac412e6d0f856e42f8473ff8
814657780 <18037818812@163.com>
2021-11-14 22:43:46 +0800

    vue3.0+TS

--- TODO-vue3.0/vue3-todo/src/store/mutations.ts
+++ TODO-vue3.0/vue3-todo/src/store/mutations.ts
@@ -1,24 +1,31 @@
 import { IState, ITodo, TODO_STATUS } from '@/typings'
-import { SET_TODO, SET_TODO_LIST, REMOVE_TODO,SET_TODO_STATUS } from './actionTypes'
+import { SET_TODO, SET_TODO_LIST, REMOVE_TODO, SET_TODO_STATUS } from './actionTypes'
 export default {
-    [SET_TODO](state:IState, todo:ITodo):void{
+    [SET_TODO](state: IState, todo: ITodo): void {
         state.list.unshift(todo);
         console.log(state.list);
     },
-    [SET_TODO_LIST](state:IState, todoList:ITodo[]):void{
+    [SET_TODO_LIST](state: IState, todoList: ITodo[]): void {
         state.list = todoList;
     },
-    [REMOVE_TODO](state:IState, id:number):void{
-        state.list = state.list.filter((item:ITodo)=>item.id!==id);
+    [REMOVE_TODO](state: IState, id: number): void {
+        state.list = state.list.filter((item: ITodo) => item.id !== id);
         console.log(state.list);
     },
-    [SET_TODO_STATUS](state:IState, id:number):void{
-        state.list = state.list.map((item:ITodo)=>{
-           if(item.id === id){
-               if(item.status===TODO_STATUS.FINISHED){
-                   item.status = TODO_STATUS.WILLDO;
-               }
-           }
+    [SET_TODO_STATUS](state: IState, id: number): void {
+        state.list = state.list.map((item: ITodo) => {
+            if (item.id === id) {
+                switch (item.status) {
+                    case TODO_STATUS.FINISHED:
+                        item.status = TODO_STATUS.WILLDO;
+                        break;
+                    case TODO_STATUS.WILLDO:
+                        item.status = TODO_STATUS.FINISHED;
+                        break;
+                    default:
+                        break;
+                }
+            }
         });
     }
 }
