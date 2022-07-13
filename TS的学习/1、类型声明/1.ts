9720c76fc4f19259dfe02f097216feb552815695
814657780 <18037818812@163.com>
2022-07-13 22:00:45 +0800

    ts的复习

--- TS的学习/1、类型声明/1.ts
+++ TS的学习/1、类型声明/1.ts
@@ -5,3 +5,13 @@ let a:string;
 a = '10';
 
 // 2、声明类型的同时，声明初始化的值。
+let b:number = 100;
+b = 101;
+
+// 3、声明初始化值的同时，声明类型
+let c = 100;
+
+function fn(a:number,b:number):number {
+    return a + b;
+}
+
